9b66668e6fa407b5368a313e4d24bd492dc92ea0
IoanaJoanne <machedonioana@gmail.com>
2018-09-07 20:36:47 +0300

    First view binded with the REST api

---
+++ Concedii/src/main/java/com/ioana/demo/controllers/MVCController.java
@@ -0,0 +1,22 @@
+package com.ioana.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MVCController {
+	// spring MVC
+		@RequestMapping(value = "/register", method = RequestMethod.GET)
+		public String displayRegisterForm() {
+
+			return "register";
+		}
+
+		@RequestMapping(value = "/CompanyCalendar", method = RequestMethod.GET)
+		public String displayFrontPage() {
+
+			return "CompanyCalendar";
+		}
+
+}
